ba2a2b5eee6cde33794ba1ea585af9af49643150
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-05-21 13:44:47 -0700

    working solution

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-window.onload = function () {
-    console.log('hey')
-};
+document.getElementById("my-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const handle = document.getElementById("handle").value;
+
+    const formData = {
+        name: name,
+        email: email,
+        handle: handle
+    };
+
+    console.log(formData);
+
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "#1E1E1E";
+});
